560e7c80ca7bc0afb4271d3d6bb5c7cb0e90ef32
Isaac <okekeisaacs@gmail.com>
2021-08-11 09:16:57 +0100

    Modified view,url and templates

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -24,7 +25,6 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
