c13b81f2f05b0c5faecfb1ee7e9b3ee27d73ac3d
Isaac <okekeisaacs@gmail.com>
2021-08-11 01:31:23 +0100

    modified views

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,6 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
